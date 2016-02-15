198b7cee901885cd05d3f81f6f8c61795c4cd025
Jishnu Mohan <jishnu7@gmail.com>
2016-02-15 17:48:59 +0530

    use the new image path

--- src/views/editor/constants/menuConstants.js
+++ src/views/editor/constants/menuConstants.js
@@ -20,8 +20,8 @@ constants.BUTTON_SLICES = {
 
 constants.BUTTONS = {
 	BLUE: {
-		UP: 'resources/images/ui/button1Up.png',
-		DOWN: 'resources/images/ui/button1Down.png',
+		UP: 'modules/adventuremap/images/ui/button1Up.png',
+		DOWN: 'modules/adventuremap/images/ui/button1Down.png',
 		FONT_FAMILY: 'BPReplay',
 		FONT_SIZE: 36,
 		COLOR: 'rgb(255, 255, 255)',
@@ -29,8 +29,8 @@ constants.BUTTONS = {
 		STROKE_WIDTH: 6
 	},
 	GREEN: {
-		UP: 'resources/images/ui/button2Up.png',
-		DOWN: 'resources/images/ui/button2Down.png',
+		UP: 'modules/adventuremap/images/ui/button2Up.png',
+		DOWN: 'modules/adventuremap/images/ui/button2Down.png',
 		FONT_FAMILY: 'BPReplay',
 		FONT_SIZE: 36,
 		COLOR: 'rgb(255, 255, 255)',
@@ -38,8 +38,8 @@ constants.BUTTONS = {
 		STROKE_WIDTH: 6
 	},
 	RED: {
-		UP: 'resources/images/ui/button3Up.png',
-		DOWN: 'resources/images/ui/button3Down.png',
+		UP: 'modules/adventuremap/images/ui/button3Up.png',
+		DOWN: 'modules/adventuremap/images/ui/button3Down.png',
 		FONT_FAMILY: 'BPReplay',
 		FONT_SIZE: 36,
 		COLOR: 'rgb(255, 255, 255)',
