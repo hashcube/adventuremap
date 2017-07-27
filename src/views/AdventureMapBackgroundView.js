48fd4f190ce64857cc8bfef2803ccb8e9a6edb96
Ramprasad Rajendran <r.ramprasad@gmail.com>
2017-07-27 17:56:51 +0530

    Handle when try to release non existent views

--- src/views/AdventureMapBackgroundView.js
+++ src/views/AdventureMapBackgroundView.js
@@ -35,26 +35,28 @@ exports = Class(AdventureMapLayerView, function (supr) {
     });
     view.update(null, x, y);
     return view;
-  }
+  };
 
   this.release = function (x, y) {
-    pool.releaseView(this._views[y][x]);
-    this._views[y][x] = null;
-  }
+    if(this._views[y] && this._views[y][x]) {
+      pool.releaseView(this._views[y][x]);
+      this._views[y][x] = null;
+    }
+  };
 
   this.create = function (x, y) {
     if (!this._views[y]) {
       this._views[y] = [];
     }
     this._views[y][x] = this.setTile(x, y);
-  }
+  };
 
   this.populateView = function (data) {
     var grid = data.grid;
     var width = this._gridSettings.width;
     var height = this._gridSettings.height;
     var tileWidth = this._tileSettings.tileWidth;
-    var tileHeight = this._tileSettings.tileHeight
+    var tileHeight = this._tileSettings.tileHeight;
     var margin = this._editMode ? 8 : 0;
     var pos = data.pos;
 
